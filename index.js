473638770c8aae3a7b4d2711d77c1aa8d6d734fa
xaviguasch <xaviguasch@gmail.com>
2020-08-18 23:43:15 +0200

    solved first challenge

--- index.js
+++ index.js
@@ -3,3 +3,27 @@
 
     Use FlexBox to create a Responsive 5x5 Grid displaying the colors    
 */
+
+function displayColors(colors) {
+  let myColorsHtml = colors
+    .map((color) => {
+      return `<div class="my-color" style="background-color: ${color.value}"></div>`
+    })
+    .join('')
+
+  document.body.innerHTML = `
+    <div class="my-colors">
+      ${myColorsHtml}
+    </div>
+    `
+}
+
+async function getColors() {
+  let response = await fetch('https://api.noopschallenge.com/hexbot?count=25')
+  let data = await response.json()
+  let colors = data.colors
+
+  displayColors(colors)
+}
+
+getColors()
